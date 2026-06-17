ec44e53d3e4292aab529bc6728364b2c119ddc3c
gfalcoperez <g.falcoperez@edu.gva.es>
2026-06-17 19:37:19 +0200

    Support string literal parameters using imageLiteral=1 annotation

--- main.ts
+++ main.ts
@@ -7,39 +7,40 @@ namespace UScreen {
      */
     //% blockId=screen_is_on_screen
     //% block="is on screen $i"
-    //% i.shadow="device_build_image"
+    //% imageLiteral=1
     //% weight=99
-    export function isOnScreen(i: Image): boolean {
+    export function isOnScreen(i: string): boolean {
 
-        if (i.pixel(0, 0) != led.point(0, 0)) return false;
-        if (i.pixel(1, 0) != led.point(1, 0)) return false;
-        if (i.pixel(2, 0) != led.point(2, 0)) return false;
-        if (i.pixel(3, 0) != led.point(3, 0)) return false;
-        if (i.pixel(4, 0) != led.point(4, 0)) return false;
+        let im = <Image><any>i;
+        if (im.pixel(0, 0) != led.point(0, 0)) return false;
+        if (im.pixel(1, 0) != led.point(1, 0)) return false;
+        if (im.pixel(2, 0) != led.point(2, 0)) return false;
+        if (im.pixel(3, 0) != led.point(3, 0)) return false;
+        if (im.pixel(4, 0) != led.point(4, 0)) return false;
 
-        if (i.pixel(0, 1) != led.point(0, 1)) return false;
-        if (i.pixel(1, 1) != led.point(1, 1)) return false;
-        if (i.pixel(2, 1) != led.point(2, 1)) return false;
-        if (i.pixel(3, 1) != led.point(3, 1)) return false;
-        if (i.pixel(4, 1) != led.point(4, 1)) return false;
+        if (im.pixel(0, 1) != led.point(0, 1)) return false;
+        if (im.pixel(1, 1) != led.point(1, 1)) return false;
+        if (im.pixel(2, 1) != led.point(2, 1)) return false;
+        if (im.pixel(3, 1) != led.point(3, 1)) return false;
+        if (im.pixel(4, 1) != led.point(4, 1)) return false;
 
-        if (i.pixel(0, 2) != led.point(0, 2)) return false;
-        if (i.pixel(1, 2) != led.point(1, 2)) return false;
-        if (i.pixel(2, 2) != led.point(2, 2)) return false;
-        if (i.pixel(3, 2) != led.point(3, 2)) return false;
-        if (i.pixel(4, 2) != led.point(4, 2)) return false;
+        if (im.pixel(0, 2) != led.point(0, 2)) return false;
+        if (im.pixel(1, 2) != led.point(1, 2)) return false;
+        if (im.pixel(2, 2) != led.point(2, 2)) return false;
+        if (im.pixel(3, 2) != led.point(3, 2)) return false;
+        if (im.pixel(4, 2) != led.point(4, 2)) return false;
 
-        if (i.pixel(0, 3) != led.point(0, 3)) return false;
-        if (i.pixel(1, 3) != led.point(1, 3)) return false;
-        if (i.pixel(2, 3) != led.point(2, 3)) return false;
-        if (i.pixel(3, 3) != led.point(3, 3)) return false;
-        if (i.pixel(4, 3) != led.point(4, 3)) return false;
+        if (im.pixel(0, 3) != led.point(0, 3)) return false;
+        if (im.pixel(1, 3) != led.point(1, 3)) return false;
+        if (im.pixel(2, 3) != led.point(2, 3)) return false;
+        if (im.pixel(3, 3) != led.point(3, 3)) return false;
+        if (im.pixel(4, 3) != led.point(4, 3)) return false;
 
-        if (i.pixel(0, 4) != led.point(0, 4)) return false;
-        if (i.pixel(1, 4) != led.point(1, 4)) return false;
-        if (i.pixel(2, 4) != led.point(2, 4)) return false;
-        if (i.pixel(3, 4) != led.point(3, 4)) return false;
-        if (i.pixel(4, 4) != led.point(4, 4)) return false;
+        if (im.pixel(0, 4) != led.point(0, 4)) return false;
+        if (im.pixel(1, 4) != led.point(1, 4)) return false;
+        if (im.pixel(2, 4) != led.point(2, 4)) return false;
+        if (im.pixel(3, 4) != led.point(3, 4)) return false;
+        if (im.pixel(4, 4) != led.point(4, 4)) return false;
 
         return true;
     }
--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
 // tests go here; this will not be compiled when this package is used as an extension.
-let img = images.createImage(`
+let check = UScreen.isOnScreen(`
 . . . . .
 . . . . .
 . . . . .
 . . . . .
 . . . . .
 `)
-let check = UScreen.isOnScreen(img)
+
