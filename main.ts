f02cd0b6a7043ab7d16d2d768a79124070abf2db
gfalcoperez <g.falcoperez@edu.gva.es>
2026-06-18 08:16:53 +0200

    refactor: simplify pixel comparison logic in isOnScreen using nested loops

--- main.ts
+++ main.ts
@@ -11,36 +11,11 @@ namespace SuperLED {
     export function isOnScreen(i: string): boolean {
 
         let im = <Image><any>i;
-        if (im.pixel(0, 0) != led.point(0, 0)) return false;
-        if (im.pixel(1, 0) != led.point(1, 0)) return false;
-        if (im.pixel(2, 0) != led.point(2, 0)) return false;
-        if (im.pixel(3, 0) != led.point(3, 0)) return false;
-        if (im.pixel(4, 0) != led.point(4, 0)) return false;
-
-        if (im.pixel(0, 1) != led.point(0, 1)) return false;
-        if (im.pixel(1, 1) != led.point(1, 1)) return false;
-        if (im.pixel(2, 1) != led.point(2, 1)) return false;
-        if (im.pixel(3, 1) != led.point(3, 1)) return false;
-        if (im.pixel(4, 1) != led.point(4, 1)) return false;
-
-        if (im.pixel(0, 2) != led.point(0, 2)) return false;
-        if (im.pixel(1, 2) != led.point(1, 2)) return false;
-        if (im.pixel(2, 2) != led.point(2, 2)) return false;
-        if (im.pixel(3, 2) != led.point(3, 2)) return false;
-        if (im.pixel(4, 2) != led.point(4, 2)) return false;
-
-        if (im.pixel(0, 3) != led.point(0, 3)) return false;
-        if (im.pixel(1, 3) != led.point(1, 3)) return false;
-        if (im.pixel(2, 3) != led.point(2, 3)) return false;
-        if (im.pixel(3, 3) != led.point(3, 3)) return false;
-        if (im.pixel(4, 3) != led.point(4, 3)) return false;
-
-        if (im.pixel(0, 4) != led.point(0, 4)) return false;
-        if (im.pixel(1, 4) != led.point(1, 4)) return false;
-        if (im.pixel(2, 4) != led.point(2, 4)) return false;
-        if (im.pixel(3, 4) != led.point(3, 4)) return false;
-        if (im.pixel(4, 4) != led.point(4, 4)) return false;
-
+        for (let k = 0; k < 5; k++){
+            for (let l = 0; l < 5; l++) {
+                if (im.pixel(k, l) != led.point(k, l)) return false;
+            }
+        }
         return true;
     }
 }
